33af245d9cc81819dcc10c1227241afa9b597751
stap780 <panaet80@mail.ru>
2022-11-22 12:39:47 +0300

    update excel

--- app/services/import.rb
+++ app/services/import.rb
@@ -54,6 +54,7 @@ class Services::Import
     require 'open-uri'
     puts "=====>>>> СТАРТ import excel_price #{Time.now.to_s}"
     excel_price.update!(file_status: false)
+    File.delete("#{Rails.public_path}/#{excel_price.id.to_s}_file.xlsx") if File.file?("#{Rails.public_path}/#{excel_price.id.to_s}_file.xlsx").present?
     url = excel_price.link
 		filename = url.split('/').last
     download = open(url)
@@ -74,16 +75,20 @@ class Services::Import
     
     s = wb.styles
     header = s.add_style bg_color: 'DD', sz: 16, b: true, alignment: { horizontal: :center, vertical: :center }
-    header_second =  s.add_style bg_color: 'DD', sz: 14, b: true, alignment: { horizontal: :center, vertical: :center }
+    header_second =  s.add_style bg_color: 'E6F1F1', sz: 14, b: true, alignment: { horizontal: :center, vertical: :center }
     tbl_header = s.add_style b: true, alignment: { horizontal: :center, vertical: :center  }
-    ind_header = s.add_style bg_color: 'E4DBEF', sz: 16, b: true, alignment: { horizontal: :center, vertical: :center , indent: 1 }
+    ind_header = s.add_style bg_color: 'CDE3E3', sz: 16, b: true, alignment: { horizontal: :center, vertical: :center , indent: 1 }
     col_header = s.add_style bg_color: 'FFDFDEDF', b: true, alignment: { horizontal: :center , vertical: :center }
     label      = s.add_style alignment: { indent: 1 }
     money      = s.add_style alignment: { horizontal: :center , vertical: :center }, format_code: "# ##0\ ₽", border: Axlsx::STYLE_THIN_BORDER #num_fmt: 7
     main_label = s.add_style bg_color: 'F2F2F2', alignment: { horizontal: :center, vertical: :center, indent: 0, wrap_text: true }
     pr_title   = s.add_style alignment: { horizontal: :left , vertical: :center, indent: 1, wrap_text: true }, border: Axlsx::STYLE_THIN_BORDER
     pr_descr   = s.add_style alignment: { horizontal: :left , vertical: :center, wrap_text: true }, border: Axlsx::STYLE_THIN_BORDER
-    pr_pict    = s.add_style border: Axlsx::STYLE_THIN_BORDER
+    pr_pict    = s.add_style alignment: { horizontal: :center , vertical: :center },border: Axlsx::STYLE_THIN_BORDER
+    pr_index   = s.add_style alignment: { horizontal: :center , vertical: :center, wrap_text: true }, border: Axlsx::STYLE_THIN_BORDER
+    back_button = s.add_style alignment: { horizontal: :center , vertical: :center, wrap_text: true }, bg_color: 'B4D5D5', sz: 14
+    bg_w = s.add_style bg_color: 'FFFFFF'
+    but_rekv = s.add_style bg_color: 'FFFFFF', alignment: { horizontal: :left , vertical: :top, indent: 1, wrap_text: true }
 
 
     start_array_string = {0=>'B6',1=>'D6',2=>'F6',3=>'H6',4=>'B8',5=>'D8',6=>'F8',7=>'H8',8=>'B10',9=>'D10',10=>'F10',11=>'H10'}
@@ -91,11 +96,11 @@ class Services::Import
     start_array = {0=>[1,5],1=>[3,5],2=>[5,5],3=>[7,5],4=>[1,7],5=>[3,7],6=>[5,7],7=>[7,7],8=>[1,9],9=>[3,9],10=>[5,9],11=>[7,9]}
     end_array = {0=>[2,6],1=>[4,6],2=>[6,6],3=>[8,6],4=>[2,8],5=>[4,8],6=>[6,8],7=>[8,8],8=>[2,10],9=>[4,10],10=>[6,10],11=>[8,10]}
     wb.add_worksheet(name: 'Main') do |sheet|
-      sheet.add_row [''], height: 30
-      sheet.add_row [''], height: 30
-      sheet.add_row [''], height: 30
-      sheet.add_row ['Каталог продукции'], height: 50, style: header
-      sheet.add_row [''], height: 10
+      sheet.add_row ['','','','','','','','','','',''], height: 30, style: bg_w
+      sheet.add_row ['','','','','','','','','','',''], height: 30, style: bg_w
+      sheet.add_row ['','','','','','','','','','',''], height: 30, style: bg_w
+      sheet.add_row ['','Каталог продукции','','','','','','','','Реквизиты',''], height: 50, style: [bg_w,header,bg_w,bg_w,bg_w,bg_w,bg_w,bg_w,bg_w,header,bg_w]
+      sheet.add_row ['','','','','','','','','','',''], height: 10, style: bg_w
       # categories_for_list.each_with_index do |cat, index|
       #   # puts "index - "+index.to_s
       #   # puts "start_array index - "+start_array[index].to_s
@@ -106,9 +111,10 @@ class Services::Import
       puts "count_rows - "+count_rows.to_s
       puts "start create main sheet rows"
       Array(0..count_rows).each do |arr|
-        sheet.add_row ['','','','','','','',''], height: 150
-        sheet.add_row ['','','','','','','',''], height: 40, style: [nil,main_label,nil,main_label,nil,main_label,nil,main_label]
+        sheet.add_row ['','','','','','','','','','',''], height: 150, style: bg_w
+        sheet.add_row ['','','','','','','','','','',''], height: 40, style: [bg_w,nil,bg_w,nil,bg_w,nil,bg_w,nil,bg_w,bg_w,bg_w]
       end
+      sheet.add_row ['','','','','','','','','','',''], height: 80, style: bg_w
       puts "finish create main sheet rows"
       puts "start add collections to main sheet"
       categories_for_list.each_with_index do |cat, index|
@@ -118,6 +124,7 @@ class Services::Import
           row_end = start_array[index][1]
 
           sheet.rows[row_end+1].cells[column_end].value = cat[:title]
+          sheet.rows[row_end+1].cells[column_end].style = main_label
           file_name = cat[:id]
           image = Services::Import.load_convert_image(cat[:image], file_name)
           # puts "image -"+image
@@ -132,10 +139,38 @@ class Services::Import
           end
           sheet.add_hyperlink( location: "'#{cat[:title].at(0..30)}'!A1", target: :sheet, ref: sheet.rows[row_end+1].cells[column_end] )
       end
-      puts "finish add collections to main sheet"
 
-      sheet.column_widths 2,25,2,25,2,25,2,25,2
-      sheet.merge_cells('A4:I4')
+      sheet.column_widths 2,25,2,25,2,25,2,25,10,50,10
+      sheet.merge_cells('B4:H4')
+      sheet.merge_cells('J6:J11')
+      image = Services::Import.load_convert_image('https://static.insales-cdn.com/assets/1/5387/1651979/1666857192/logo.png', 'logo')
+      sheet.add_image(image_src: image, start_at: 'B1', end_at: 'E4')
+      sheet['J6'].value = 'Общество с ограниченной ответственностью «Адвентер»
+
+188802, ЛО, г.Выборг, ул. Данилова, д.15 корп.1, оф.248
+      
+Тел. 89218800978
+эл.почта: info@adventer.su
+Сайт:  www.adventer.su
+      
+ИНН: 4704097388
+КПП: 470401001
+ОГРН: 1154704001264
+ОКПО: 23384032
+ОКАТО: 41417000000
+ОКВЭД: 46.49, 73.11
+ОКОГУ: 4210014
+ОКТМО: 41615101001
+      
+Расчетный счет: 40702810555390000762
+Кор. счет: 30101810500000000653
+БИК: 044030653
+Банк: Северо-Западный банк ПАО Сбербанк г. Санкт-Петербург
+      
+      
+Упрощенная система налогообложения  – без НДС.'
+      sheet['J6'].style = but_rekv
+      puts "finish add collections to main sheet"
     end
 
     row_index_for_titles_array = []
@@ -143,19 +178,24 @@ class Services::Import
     categories_for_list.each_with_index do |cat, index|
       puts "start create sheet - "+cat[:title]
         wb.add_worksheet(name: cat[:title].at(0..30)) do |sheet|
-          sheet.add_row ["<= НА ГЛАВНУЮ",'', cat[:title]], style: [nil, nil, ind_header]
+          sheet.add_row ["<= НА ГЛАВНУЮ",'','', cat[:title]], style: [back_button,back_button,back_button,ind_header], height: 30
           second_cats = all_categories.select{ |c| c[:parent_id] == cat[:id] }
           if second_cats.present?
             second_cats.each do |s_cat|
               cat_title_row = sheet.add_row ['',s_cat[:title]], style: header_second, height: 30
               row_index_for_titles_array.push(cat_title_row.row_index+1)
-              sheet.add_row ['','Название','Описание','Картинка','Цена'], style: tbl_header, height: 20
-              cat_products = Rails.env.development? ? offers.select{|item| item.css('categoryId').text == s_cat[:id]}.take(1) : offers.select{|item| item.css('categoryId').text == s_cat[:id]}
-              cat_products.each do |pr|
+              sheet.add_row ['','№','Фото','Наименование','Артикул','Описание','Цена'], style: tbl_header, height: 20
+              cat_products = Rails.env.development? ? offers.select{|item| item.css('categoryId').text == s_cat[:id]}.take(2) : offers.select{|item| item.css('categoryId').text == s_cat[:id]}
+              cat_products.each_with_index do |pr, index|
+                title = pr.css('model').text.present? ? pr.css('model').text : ' '
+                sku = pr.css('sku').text.present? ? pr.css('sku').text : pr['id']
+                desc = pr.css('description').text.present? ? pr.css('description').text : ' '
                 price = Services::Import.price_shift(excel_price, pr.css('price').text)
-                pr_row = sheet.add_row ['',pr.css('model').text, pr.css('description').text, '', price], style: [nil, pr_title, pr_descr, pr_pict, money]
+                pr_data = ['',(index+1).to_s,'',title,sku,desc,price]
+                #puts pr_data.to_s if pr['id'] == '139020547'
+                pr_row = sheet.add_row pr_data, style: [nil,pr_index,pr_pict,pr_title,pr_title,pr_descr,money], height: 110
                 # puts "pr_row.row_index - "+pr_row.row_index.to_s
-                hyp_ref = "B#{pr_row.row_index.to_s}"
+                hyp_ref = "D#{(pr_row.row_index+1).to_s}"
                 # puts hyp_ref.to_s
                 sheet.add_hyperlink location: pr.css('url').text, ref: hyp_ref
                 file_name = pr['id']
@@ -165,10 +205,10 @@ class Services::Import
                 # puts "start_array[index].to_s - "+start_array[index].to_s
                 # puts "end_array[index].to_s - "+end_array[index].to_s
                 sheet.add_image(image_src: image, :noSelect => true, :noMove => true) do |image|
-                  image.width = 100
-                  image.height = 100
-                  image.start_at 3, pr_row.row_index
-                  image.end_at 4, pr_row.row_index+1
+                  # image.width = 100
+                  image.height = 90
+                  image.start_at 2, pr_row.row_index
+                  image.end_at 3, pr_row.row_index+1
                 end            
               end
             end
@@ -176,13 +216,18 @@ class Services::Import
           if !second_cats.present?
             cat_title_row = sheet.add_row ['',cat[:title]], style: header_second, height: 30
             row_index_for_titles_array.push(cat_title_row.row_index+1)
-            sheet.add_row ['','Название','Описание','Картинка','Цена'], style: tbl_header, height: 20
-            cat_products = Rails.env.development? ? offers.select{|item| item.css('categoryId').text == cat[:id]}.take(5) : offers.select{|item| item.css('categoryId').text == cat[:id]}
-            cat_products.each do |pr|
+            sheet.add_row ['','№','Фото','Наименование','Артикул','Описание','Цена'], style: tbl_header, height: 20
+            cat_products = Rails.env.development? ? offers.select{|item| item.css('categoryId').text == cat[:id]}.take(2) : offers.select{|item| item.css('categoryId').text == cat[:id]}
+            cat_products.each_with_index do |pr, index|
+              title = pr.css('model').text.present? ? pr.css('model').text : ' '
+              sku = pr.css('sku').text.present? ? pr.css('sku').text : pr['id']
+              desc = pr.css('description').text.present? ? pr.css('description').text : ' '
               price = Services::Import.price_shift(excel_price, pr.css('price').text)
-              pr_row = sheet.add_row ['',pr.css('model').text, pr.css('description').text, '', price], style: [nil, pr_title, pr_descr, pr_pict, money]
+              pr_data = ['',(index+1).to_s,'',title,sku,desc,price]
+              #puts pr_data.to_s if pr['id'] == '139020547'
+              pr_row = sheet.add_row pr_data, style: [nil,pr_index,pr_pict,pr_title,pr_title,pr_descr,money], height: 110
               # puts "pr_row.row_index - "+pr_row.row_index.to_s
-              hyp_ref = "B#{pr_row.row_index.to_s}"
+              hyp_ref = "D#{(pr_row.row_index+1).to_s}"
               sheet.add_hyperlink location: pr.css('url').text, ref: hyp_ref
               file_name = pr['id']
               picture = pr.css('picture').size > 1 ? pr.css('picture').first.text : pr.css('picture').text
@@ -191,20 +236,27 @@ class Services::Import
               # puts "start_array[index].to_s - "+start_array[index].to_s
               # puts "end_array[index].to_s - "+end_array[index].to_s
               sheet.add_image(image_src: image, :noSelect => true, :noMove => true) do |image|
-                image.width = 100
-                image.height = 100
-                image.start_at 3, pr_row.row_index
-                image.end_at 4, pr_row.row_index+1
+                # image.width = 100
+                image.height = 90
+                image.start_at 2, pr_row.row_index
+                image.end_at 3, pr_row.row_index+1
               end          
             end
           end
 
-          sheet.merge_cells("A1:B1")
-          sheet.merge_cells("C1:E1")
-          sheet.add_hyperlink( location: "'Main'!A1", target: :sheet,ref: 'B1' )
-          sheet.column_widths 2,40,40,40,20,2
-          merge_ranges = row_index_for_titles_array.map{|a| "B"+a.to_s+":"+"E"+a.to_s }
+          sheet.merge_cells("A1:C1")
+          sheet.merge_cells("D1:G1")
+          sheet.add_hyperlink( location: "'Main'!A7", target: :sheet, ref: 'B1' )
+          sheet.column_widths 2,10,20,40,40,40,40,2
+          merge_ranges = row_index_for_titles_array.map{|a| "B"+a.to_s+":"+"G"+a.to_s }
           merge_ranges.uniq.each { |range| sheet.merge_cells(range) }
+          sheet.sheet_view.pane do |pane|
+            pane.top_left_cell = 'B2'
+            pane.state = :frozen_split
+            pane.y_split = 1
+            pane.x_split = 1
+            pane.active_pane = :bottom_right
+          end
         end
       puts "finish create sheet - "+cat[:title]
     end
